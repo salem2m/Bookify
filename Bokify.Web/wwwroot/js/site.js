02582d528f0564206addfb6b470c82589a67af4d
salem gomaa <salemgomaa01@gmail.com>
2024-12-09 04:23:16 +0200

    Add DataTables to Categories

--- Bokify.Web/wwwroot/js/site.js
+++ Bokify.Web/wwwroot/js/site.js
@@ -1,4 +1,7 @@
-﻿function showSuccessMessage(message = 'Saved successfully') {
+﻿var table;
+var datatable;
+var exportedCols = [];
+function showSuccessMessage(message = 'Saved successfully') {
     Swal.fire({
         icon: 'success',
         title: 'success',
@@ -18,10 +21,120 @@ function showErrorMessage(message = 'Saved successfully') {
         }
     });
 }
+
+//DataTables
+var headers = $('th');
+$.each(headers, function (i) {
+    if (!$(this).hasClass('js-no-export'))
+        exportedCols.push(i);
+});
+
+// Class definition
+var KTDatatables = function () {
+    // Private functions
+    var initDatatable = function () {
+
+        datatable = $(table).DataTable({
+            "info": false,
+            'order': [],
+            'pageLength': 10,
+        });
+    }
+
+
+    // Export options
+    var exportButtons = () => {
+
+        const documentTitle = $('.js-datatables').data('document-title');
+
+        var buttons = new $.fn.dataTable.Buttons(table, {
+
+            buttons: [
+                {
+                    extend: 'copyHtml5',
+                    title: documentTitle,
+                    exportOptions: {
+                        columns: exportedCols
+                    }
+                },
+                {
+                    extend: 'excelHtml5',
+                    title: documentTitle,
+                    exportOptions: {
+                        columns: exportedCols
+                    }
+
+                },
+                {
+                    extend: 'csvHtml5',
+                    title: documentTitle,
+                    exportOptions: {
+                        columns: exportedCols
+                    }
+                },
+                {
+                    extend: 'pdfHtml5',
+                    title: documentTitle,
+                    exportOptions: {
+                        columns: exportedCols
+                    }
+                }
+            ]
+        }).container().appendTo($('#kt_datatable_example_buttons'));
+
+        // Hook dropdown menu click event to datatable export buttons
+        const exportButtons = document.querySelectorAll('#kt_datatable_example_export_menu [data-kt-export]');
+        exportButtons.forEach(exportButton => {
+            exportButton.addEventListener('click', e => {
+                e.preventDefault();
+
+                // Get clicked export value
+                const exportValue = e.target.getAttribute('data-kt-export');
+                const target = document.querySelector('.dt-buttons .buttons-' + exportValue);
+
+                // Trigger click event on hidden datatable export buttons
+                target.click();
+            });
+        });
+    }
+    // Search Datatable --- official docs reference: https://datatables.net/reference/api/search()
+    var handleSearchDatatable = () => {
+        const filterSearch = document.querySelector('[data-kt-filter="search"]');
+        filterSearch.addEventListener('keyup', function (e) {
+            datatable.search(e.target.value).draw();
+        });
+    }
+    // Public methods
+
+    return {
+
+        init: function () {
+
+            table = document.querySelector('.js-datatables');
+
+            if (!table) {
+                return;
+            }
+
+            initDatatable();
+            exportButtons();
+            handleSearchDatatable();
+
+        }
+    };
+
+}();
+
+
 $(document).ready(function() {
     var message = $('#Message').text();
     if (message !== '') {
         showSuccessMessage(message);
     }
+
+    //DataTables
+    KTUtil.onDOMContentLoaded(function () {
+        KTDatatables.init();
+    });
 
 });
